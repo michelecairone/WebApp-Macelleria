b99ea8f357d2dda20c75b7772b7e626241c1aa74
Leonardo Minaudo <leonardo.minaudo@studenti.unipr.it>
2022-05-07 23:16:35 +0200

    Orders Client

--- pages/usr/[userID]/orders.jsx
+++ pages/usr/[userID]/orders.jsx
@@ -9,39 +9,88 @@ import Container from '@mui/material/Container';
 import { useState } from "react";
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { styled } from '@mui/material/styles';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import { PrintDisabled } from "@mui/icons-material";
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
 
 
 
 export default function Orders({ user, orders }) {
-    console.log(orders.lenght);
+    console.log(orders);
+    const [product, setProduct] = useState([]);
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+
+        setExpanded(!expanded);
+    };
+
+    function Products({ order }) {
+        axios.get(`http://localhost:80/api/user/1/orders/${order}`).then(function (response) {
+            console.log(response.data);
+
+            setProduct(response.data);
+
+        });
+
+        {
+           product.map((prod) => {
+                return (
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="space-evenly"
+                        alignItems="center"
+                    >
+                        <Grid item>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 151 }}
+                                image={`/image/${prod.image}`}
+                                alt="Live from space album cover"
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="h6" gutterBottom>
+                                {prod.name}
+                            </Typography>
+                        </Grid>
+
+                        <Grid item>
+                            <Typography variant="overline" display="block" gutterBottom>
+                                {prod.amount} Kg
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="overline" display="block" gutterBottom>
+                                € {prod.total}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+
+                )
+            })
+        }
+
+
+
+    }
 
     const [order, setOrder] = useState(orders[0].id_order);
 
-    function Scheda() {
-        let i = orders.lenght;
-
-
-        return (
-            <Card sx={{ minWidth: 700 }}>
-                <CardContent>
-                    <Typography variant="h6" gutterBottom component="div">
-                        #ORDINE: {orders[i - 1].id_order}
-                    </Typography>
-                    <Typography variant="h7" component="div">
-                        Data: {orders[i - 1].date_ord}
-                    </Typography>
-                    <hr />
-                    <Box sx={{ flexGrow: 1 }}>
-
-                    </Box>
-                </CardContent>
-                <CardActions>
-                    <Button size="small">Learn More</Button>
-                </CardActions>
-            </Card>
-
-        );
-    };
 
 
     return (
@@ -55,6 +104,42 @@ export default function Orders({ user, orders }) {
                     marginBottom: 8,
                 }}
             >
+                {orders.map((ord) => (
+                    <Card sx={{ minWidth: 700 }}>
+                        <CardContent>
+                            <Typography variant="h6" gutterBottom component="div">
+                                #ORDINE: {ord.id_order}
+                            </Typography>
+                            <Typography variant="h7" component="div">
+                                Data: {ord.date_ord}
+                            </Typography>
+                            <CardActions disableSpacing>
+
+                                <ExpandMore
+                                    expand={expanded}
+                                    onClick={handleExpandClick}
+                                    aria-expanded={expanded}
+                                    aria-label="show more"
+                                >
+                                    <ExpandMoreIcon />
+                                </ExpandMore>
+                            </CardActions>
+                            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                                <CardContent>
+                                    <Box sx={{ flexGrow: 1 }}>
+                                        <Products order={ord.id_order} />
+                                    </Box>
+                                </CardContent>
+                            </Collapse>
+                            <hr />
+
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Learn More</Button>
+                        </CardActions>
+                    </Card>
+
+                ))}
                 <Card sx={{ minWidth: 700 }}>
                     <CardContent>
                         <Typography variant="h6" gutterBottom component="div">
@@ -63,50 +148,66 @@ export default function Orders({ user, orders }) {
                         <Typography variant="h7" component="div">
                             Data: {orders[0].date_ord}
                         </Typography>
+                        <CardActions disableSpacing>
+
+                            <ExpandMore
+                                expand={expanded}
+                                onClick={handleExpandClick}
+                                aria-expanded={expanded}
+                                aria-label="show more"
+                            >
+                                <ExpandMoreIcon />
+                            </ExpandMore>
+                        </CardActions>
+                        <Collapse in={expanded} timeout="auto" unmountOnExit>
+                            <CardContent>
+                                <Box sx={{ flexGrow: 1 }}>
+
+
+                                    {orders.map((ord) => (
+
+
+                                        <Grid
+                                            container
+                                            direction="row"
+                                            justifyContent="space-evenly"
+                                            alignItems="center"
+                                        >
+                                            <Grid item>
+                                                <CardMedia
+                                                    component="img"
+                                                    sx={{ width: 151 }}
+                                                    image={`/image/${ord.image}`}
+                                                    alt="Live from space album cover"
+                                                />
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography variant="h6" gutterBottom>
+                                                    {ord.name}
+                                                </Typography>
+                                            </Grid>
+
+                                            <Grid item>
+                                                <Typography variant="overline" display="block" gutterBottom>
+                                                    {ord.amount} Kg
+                                                </Typography>
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography variant="overline" display="block" gutterBottom>
+                                                    € {ord.total}
+                                                </Typography>
+                                            </Grid>
+                                        </Grid>
+
+
+
+
+                                    ))}
+                                </Box>
+                            </CardContent>
+                        </Collapse>
                         <hr />
-                        <Box sx={{ flexGrow: 1 }}>
 
-
-                            {orders.map((ord) => (
-
-
-                                <Grid
-                                    container
-                                    direction="row"
-                                    justifyContent="space-evenly"
-                                    alignItems="center"
-                                >
-                                    <Grid item>
-                                        <CardMedia
-                                            component="img"
-                                            sx={{ width: 151 }}
-                                            image={`/image/${ord.image}`}
-                                            alt="Live from space album cover"
-                                        />
-                                    </Grid>
-                                    <Grid item>
-                                        <Typography variant="h6" gutterBottom>
-                                            {ord.name}
-                                        </Typography>
-                                    </Grid>
-
-                                    <Grid item>
-                                        <Typography variant="overline" display="block" gutterBottom>
-                                            {ord.amount} Kg
-                                        </Typography>
-                                    </Grid>
-                                    <Grid item>
-                                        <Typography variant="overline" display="block" gutterBottom>
-                                            € {ord.total}
-                                        </Typography>
-                                    </Grid>
-                                </Grid>
-
-
-
-
-                            ))}
-                        </Box>
                     </CardContent>
                     <CardActions>
                         <Button size="small">Learn More</Button>
