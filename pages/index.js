d93f9c9a5245da08b6b99335592b09bc5290f69a
Leonardo Minaudo <leonardo.minaudo@studenti.unipr.it>
2022-05-12 00:07:02 +0200

    Auto stash before merge of "main" and "origin/main"

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,7 @@ import axios from "axios";
 import Head from "next/head";
 import { useState, useEffect } from "react";
 import Add from "../components/Add";
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 import Featured from "../components/Featured";
 import ProductList from "../components/ProductList";
 import styles from "../styles/Home.module.css";
