94543fa3187e6c8f703f4c1df0077421ce5bc17a
Michele Cairone <1michelecairone@gmail.com>
2022-06-16 17:49:21 +0200

    update api

--- api/index.php
+++ api/index.php
@@ -96,8 +96,6 @@ switch ($method) {
                     $stmt->execute();
                     $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 } else {
-
-
                     $sql = 'SELECT id_order, c.id, name, surname, state, date_ord, total
                       FROM clients c, orders o, make m
                       WHERE m.id_order = o.id
@@ -115,7 +113,7 @@ switch ($method) {
 
         break;
     case "POST":
-        $user = json_decode(file_get_contents('php://input'));
+        $input = json_decode(file_get_contents('php://input'));
 
         $path = explode('/', $_SERVER['REQUEST_URI']);
         if ($path[2] === 'products') {
@@ -123,13 +121,12 @@ switch ($method) {
             if ($path[3] === 'order') {
 
                 $sql = "INSERT INTO make(id_order, id_client, date_ord)
-              VALUES(null, :id_client, :date_ord)";
+                        VALUES(null, :id_client, :date_ord)";
 
                 $stmt = $conn->prepare($sql);
 
-                $id_client = $user->id_client;
+                $id_client = $input->id_client;
                 $data = date("Y-m-d H:i:s");
-
                 $stmt->bindParam(':id_client', $id_client);
                 $stmt->bindParam(':date_ord', $data);
                 $rst = $stmt->execute();
@@ -146,15 +143,15 @@ switch ($method) {
                 $objDb3 = new DbConnect;
                 $conn3 = $objDb3->connect();
                 $sql3 = "INSERT INTO orders(id, state, total)
-              VALUES(:id_order, :state, :total)";
-                $pp = "attesa di conferma";
+                            VALUES(:id_order, :state, :total)";
+                $state = "attesa di conferma";
                 $stmt3 = $conn3->prepare($sql3);
                 $stmt3->bindParam(':id_order', $id_order["id_order"]);
-                $stmt3->bindParam(':state', $pp);
-                $stmt3->bindParam(':total', $user->cart_total);
+                $stmt3->bindParam(':state', $state);
+                $stmt3->bindParam(':total', $input->cart_total);
                 $rst2 = $stmt3->execute();
 
-                $prodotti = $user->products;
+                $prodotti = $input->products;
 
                 foreach ($prodotti as $prodotto) {
 
@@ -162,7 +159,7 @@ switch ($method) {
                     $conn4 = $objDb4->connect();
 
                     $sql4 = "INSERT INTO shopping_cart(id_order, id_product, amount, total)
-                VALUES(:id_order, :id_product, :amount, :total)";
+                             VALUES(:id_order, :id_product, :amount, :total)";
 
                     $stmt4 = $conn4->prepare($sql4);
                     $total = ($prodotto->quantity * $prodotto->price);
@@ -170,22 +167,21 @@ switch ($method) {
                     $stmt4->bindParam(':id_product', $prodotto->id);
                     $stmt4->bindParam(':amount', $prodotto->quantity);
                     $stmt4->bindParam(':total', $total);
-
                     $rst3 = $stmt4->execute();
-
                 }
                 echo json_encode($id_order);
             }
             if ($path[3] === 'save') {
-                $sql = "INSERT INTO products(id, name, price, description, image, id_category)
-              VALUES(null, :name, :price, :description, :image, :id_category)";
-                $stmt = $conn->prepare($sql);
 
-                $stmt->bindParam(':name', $user->name);
-                $stmt->bindParam(':price', $user->price);
-                $stmt->bindParam(':description', $user->description);
-                $stmt->bindParam(':image', $user->image);
-                $stmt->bindParam(':id_category', $user->id_category);
+                $sql = "INSERT INTO products(id, name, price, description, image, id_category)
+                        VALUES(null, :name, :price, :description, :image, :id_category)";
+
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':name', $input->name);
+                $stmt->bindParam(':price', $input->price);
+                $stmt->bindParam(':description', $input->description);
+                $stmt->bindParam(':image', $input->image);
+                $stmt->bindParam(':id_category', $input->id_category);
 
                 if ($stmt->execute()) {
                     $response = ['status' => 1, 'message' => 'Record created successfully.'];
@@ -197,14 +193,14 @@ switch ($method) {
             if ($path[3] === 'filter') {
 
                 $sql = "SELECT p.id, p.name, p.price, p.description, p.image FROM products p, category c
-                    WHERE p.id_category = c.id";
+                        WHERE p.id_category = c.id";
                 $var = 0;
 
-                if ($user->Bianca) {
+                if ($input->Bianca) {
                     $sql .= " AND (c.name = 'Carne Bianca'";
                     $var++;
                 }
-                if ($user->Rossa) {
+                if ($input->Rossa) {
                     if ($var > 0) $sql .= " OR";
                     else {
                         $sql .= " AND (";
@@ -212,7 +208,7 @@ switch ($method) {
                     }
                     $sql .= " c.name = 'Carne Rossa'";
                 }
-                if ($user->Preparati) {
+                if ($input->Preparati) {
                     if ($var > 0) $sql .= " OR";
                     else {
                         $sql .= " AND (";
@@ -235,25 +231,25 @@ switch ($method) {
                 $sql = "SELECT * FROM clients";
                 $sql .= " WHERE email = :email ";
                 $stmt = $conn->prepare($sql);
-                $stmt->bindParam(':email', $user->email);
+                $stmt->bindParam(':email', $input->email);
                 $stmt->execute();
                 $usr = $stmt->fetch(PDO::FETCH_ASSOC);
                 echo json_encode($usr);
             }
 
             if ($path[3] === 'save') {
+
                 $sql = "INSERT INTO clients(id, name, surname, email, password, address, city, telephone)
-                VALUES(null, :name, :surname, :email, :password, :address, :city, :telephone)";
+                        VALUES(null, :name, :surname, :email, :password, :address, :city, :telephone)";
+
                 $stmt = $conn->prepare($sql);
-
-                $stmt->bindParam(':name', $user->name);
-                $stmt->bindParam(':surname', $user->surname);
-                $stmt->bindParam(':email', $user->email);
-                $stmt->bindParam(':password', $user->password);
-                $stmt->bindParam(':address', $user->address);
-                $stmt->bindParam(':city', $user->city);
-                $stmt->bindParam(':telephone', $user->telephone);
-
+                $stmt->bindParam(':name', $input->name);
+                $stmt->bindParam(':surname', $input->surname);
+                $stmt->bindParam(':email', $input->email);
+                $stmt->bindParam(':password', $input->password);
+                $stmt->bindParam(':address', $input->address);
+                $stmt->bindParam(':city', $input->city);
+                $stmt->bindParam(':telephone', $input->telephone);
 
                 if ($stmt->execute()) {
                     $response = ['status' => 1, 'message' => 'Record created successfully.'];
@@ -264,27 +260,26 @@ switch ($method) {
             }
         }
 
-
         break;
 
     case "PUT":
-        $user = json_decode(file_get_contents('php://input'));
+        $input = json_decode(file_get_contents('php://input'));
         $path = explode('/', $_SERVER['REQUEST_URI']);
+
         if ($path[2] === 'user') {
             if (isset($path[3]) && is_numeric($path[3]) && isset($path[4]) && ($path[4] === 'edit')) {
                 $sql = "UPDATE clients
                         SET name= :name, surname = :surname, address = :address, city= :city, email =:email, password= :password, telephone =:telephone WHERE clients.id = :id";
                 $stmt = $conn->prepare($sql);
 
-                $stmt->bindParam(':id', $user->id);
-                $stmt->bindParam(':name', $user->name);
-                $stmt->bindParam(':surname', $user->surname);
-                $stmt->bindParam(':address', $user->address);
-                $stmt->bindParam(':city', $user->city);
-                $stmt->bindParam(':email', $user->email);
-                $stmt->bindParam(':password', $user->password);
-                $stmt->bindParam(':telephone', $user->telephone);
-
+                $stmt->bindParam(':id', $input->id);
+                $stmt->bindParam(':name', $input->name);
+                $stmt->bindParam(':surname', $input->surname);
+                $stmt->bindParam(':address', $input->address);
+                $stmt->bindParam(':city', $input->city);
+                $stmt->bindParam(':email', $input->email);
+                $stmt->bindParam(':password', $input->password);
+                $stmt->bindParam(':telephone', $input->telephone);
 
                 if ($stmt->execute()) {
                     $response = ['status' => 1, 'message' => 'Record updated successfully.'];
@@ -297,17 +292,17 @@ switch ($method) {
         if ($path[2] === 'product') {
 
             if (isset($path[3]) && is_numeric($path[3]) && isset($path[4]) && ($path[4] === 'edit')) {
+
                 $sql = "UPDATE products
                         SET name= :name, price = :price, description= :description, image =:image, id_category =:id_category WHERE products.id = :id";
+
                 $stmt = $conn->prepare($sql);
-
                 $stmt->bindParam(':id', $path[3]);
-                $stmt->bindParam(':name', $user->name);
-                $stmt->bindParam(':price', $user->price);
-                $stmt->bindParam(':description', $user->description);
-                $stmt->bindParam(':image', $user->image);
-                $stmt->bindParam(':id_category', $user->id_category);
-
+                $stmt->bindParam(':name', $input->name);
+                $stmt->bindParam(':price', $input->price);
+                $stmt->bindParam(':description', $input->description);
+                $stmt->bindParam(':image', $input->image);
+                $stmt->bindParam(':id_category', $input->id_category);
 
                 if ($stmt->execute()) {
                     $response = ['status' => 1, 'message' => 'Record updated successfully.'];
@@ -320,22 +315,21 @@ switch ($method) {
 
         if ($path[2] === 'order_state') {
 
-            if (isset($path[3]) && is_numeric($path[3]) ) {
+            if (isset($path[3]) && is_numeric($path[3])) {
+
                 $sql = "UPDATE orders
                         SET state= :state WHERE orders.id = :id";
-                $stmt = $conn->prepare($sql);
 
+                $stmt = $conn->prepare($sql);
                 $stmt->bindParam(':id', $path[3]);
 
-                if ($user === 1){
+                if ($input === 1) {
                     $stmt->bindValue(':state', "in preparazione", PDO::PARAM_STR);
-                }
-                else if ($user === 2){
+                } else if ($input === 2) {
                     $stmt->bindValue(':state', "in consegna", PDO::PARAM_STR);
-                } else if ($user === 3){
+                } else if ($input === 3) {
                     $stmt->bindValue(':state', "consegnato", PDO::PARAM_STR);
                 }
-
 
                 if ($stmt->execute()) {
                     $response = ['status' => 1, 'message' => 'State updated successfully.'];
@@ -349,6 +343,7 @@ switch ($method) {
         break;
 
     case "DELETE":
+        
         $path = explode('/', $_SERVER['REQUEST_URI']);
 
         if ($path[2] === 'products') {
